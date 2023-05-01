091dcb78f54d978a60fb9b412fcd225713ec9ad0
Viki Mancoridis <vmancoridis@gmail.com>
2023-05-01 17:12:39 -0400

    i think this is all intrinsics/extrinsics

--- nerf_llff_data/example_data_load.py
+++ nerf_llff_data/example_data_load.py
@@ -49,3 +49,26 @@ intrinsic_mat = np.array([[focal_length, 0.0, 0.5 * hwf[1]],
                           [0.0, focal_length * aspect_ratio, 0.5 * hwf[0]],
                           [0.0, 0.0, 1.0]])
 print("intrinsic matrix\n", intrinsic_mat)
+
+############################ PIXEL SIZE ################################
+
+# The default value for sensor_size is set to 36, which corresponds to a
+#  full-frame camera sensor size of 36mm x 24mm. 
+# Why 36? --> corresponds to the full-frame sensor size used in many 
+# professional DSLR cameras
+# focal length is also given in mm
+
+sensor_size = 36
+pixel_size = sensor_size / np.array([W, H]) * focal_length
+# This gives the pixel size in millimeters per pixel.
+
+# Explanation of the formula...
+
+# The formula for pixel_size assumes that the camera sensor is rectangular 
+# and that the image has the same aspect ratio as the sensor. This means 
+# that the ratio of the sensor width to the sensor height is the same as 
+# the ratio of the image width to the image height. With this assumption,
+#  we can compute the physical size of a single pixel on the sensor by 
+# dividing the sensor size by the number of pixels along each dimension, 
+# and then scaling by the focal length to account for the projection of 
+# the image onto the sensor.
