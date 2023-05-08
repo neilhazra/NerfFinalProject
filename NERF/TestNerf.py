2030b4f70d54b0e9ac94210a5de32e718098fc0a
saumyamalik <saumyam@princeton.edu>
2023-05-08 16:18:14 -0400

    test updates

--- NERF/TestNerf.py
+++ NERF/TestNerf.py
@@ -12,7 +12,7 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
 
 
 model = NerfModel().cuda()
-model.load_state_dict(torch.load("./model_256/nerf_model_50000_.model"))
+model.load_state_dict(torch.load("./model_50k_positional(L5)/nerf_model_60000_.model"))
 
 images, _, poses, _, [H, W, focal], _ = load_blender_data_test_depth("./data/drums")
 
@@ -23,8 +23,10 @@ homogenous_pixel_coordinates = torch.stack([homogenous_pixels_x, homogenous_pixe
 print(images.shape, poses.shape, intrinsics)
 print('generating rays')
 
+losses = torch.zeros(200)
 with torch.no_grad():
-    for i in [131]:
+    #for i in range(200):
+    for i in [199]:
         print('processing image', i)
         translation, world_direction = pixel_intrinsics_extrinsics_to_ray(homogenous_pixel_coordinates, intrinsics, torch.Tensor(poses[i][:3,:])) 
         rendered_color_splits = []
@@ -33,13 +35,23 @@ with torch.no_grad():
             split_world_direction = world_direction.reshape(-1,3)[split*160000: (split+1)*160000]
             rendered_colors = training_forward_pass(model, split_translation.cuda(), split_world_direction.cuda(), 2, 6, 100)
             rendered_color_splits.append(rendered_colors)
-            
+        
+        # SUM OVER RGB, AVERAGE OVER NUM PIXELS
         rendered_colors = torch.cat(rendered_color_splits, dim=0).reshape(800,800,3)    
+
+        # COMPUTING AVERAGE LOSS
+        # iter_loss = torch.sum((rendered_colors.cpu() - images[i][:,:,:3])**2)
+        # avg_loss = iter_loss / (800*800)
+        # losses[i] = avg_loss
+        # print(avg_loss)
+        
+        # DISPLAYING IMAGE
         print('we have an image', rendered_colors.detach().cpu().numpy().max())
         print('real image', images[i].mean())
-        cv2.imwrite('test_L10_256_50k.png', np.flip((rendered_colors.detach().cpu().numpy()*255).astype(int), axis=-1))
+        cv2.imwrite('test_199_N1_60k_L5.png', np.flip((rendered_colors.detach().cpu().numpy()*255).astype(int), axis=-1))
         cv2.imwrite('actual.png', np.flip((images[i][...,:3]*255).astype(int), axis=-1))
         
-        break
+    
+    print("Average loss:", losses.mean())
     
     
--- NERF/TestObjectNerf.py
+++ NERF/TestObjectNerf.py
@@ -12,7 +12,7 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:256"
 
 
 model = ObjectNerf().cuda()
-model.load_state_dict(torch.load("./model_positional_L10/nerf_model_20000_.model"))
+model.load_state_dict(torch.load("./model_object_N2/nerf_model_90000_.model"))
 
 images, _, poses, _, [H, W, focal], _ = load_blender_data_test_depth("./data/drums")
 
@@ -23,23 +23,31 @@ homogenous_pixel_coordinates = torch.stack([homogenous_pixels_x, homogenous_pixe
 print(images.shape, poses.shape, intrinsics)
 print('generating rays')
 
+losses = torch.zeros(200)
 with torch.no_grad():
-    for i in [131]:
+    for i in range(200):
+    #for i in [131]:
         print('processing image', i)
         translation, world_direction = pixel_intrinsics_extrinsics_to_ray(homogenous_pixel_coordinates, intrinsics, torch.Tensor(poses[i][:3,:])) 
         rendered_color_splits = []
         for split in range(4):
             split_translation = translation.expand_as(world_direction).reshape(-1,3)[split*160000: (split+1)*160000]
             split_world_direction = world_direction.reshape(-1,3)[split*160000: (split+1)*160000]
-            rendered_colors, _ = model(split_translation.cuda(), split_world_direction.cuda(), 2, 6, 100)
+            rendered_colors, _ = model.forward_eval(split_translation.cuda(), split_world_direction.cuda(), 2, 6, 100)
             rendered_color_splits.append(rendered_colors)
             
         rendered_colors = torch.cat(rendered_color_splits, dim=0).reshape(800,800,3)    
-        print('we have an image', rendered_colors.detach().cpu().numpy().max())
-        print('real image', images[i].mean())
-        cv2.imwrite('131.png', np.flip((rendered_colors.detach().cpu().numpy()*255).astype(int), axis=-1))
-        cv2.imwrite('actual.png', np.flip((images[i][...,:3]*255).astype(int), axis=-1))
+        # print('we have an image', rendered_colors.detach().cpu().numpy().max())
+        # print('real image', images[i].mean())
+        # cv2.imwrite('test_131_N2_90k.png', np.flip((rendered_colors.detach().cpu().numpy()*255).astype(int), axis=-1))
+        # cv2.imwrite('actual.png', np.flip((images[i][...,:3]*255).astype(int), axis=-1))
         
-        break
+        # COMPUTING AVERAGE LOSS
+        iter_loss = torch.sum((rendered_colors.cpu() - images[i][:,:,:3])**2)
+        avg_loss = iter_loss / (800*800)
+        losses[i] = avg_loss
+        print(avg_loss)
+        
+        
     
     
